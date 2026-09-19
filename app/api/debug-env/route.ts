e82cd9dfe775eed700c0e84215304afadee62325
crackerking <crackerking@naver.com>
2026-09-19 16:41:01 +0900

    update debug to fetch bus api

--- app/api/debug-env/route.ts
+++ app/api/debug-env/route.ts
@@ -1,13 +1,15 @@
 import { NextResponse } from 'next/server';
 
 export async function GET() {
+  const serviceKey = process.env.BUS_API_SERVICE_KEY;
+  let busResponse = '';
+  try {
+    const res = await fetch('http://apis.data.go.kr/6290000/busrouteinfo/getrouteinfo?serviceKey=' + serviceKey);
+    busResponse = await res.text();
+  } catch(e: any) {
+    busResponse = e.message;
+  }
   return NextResponse.json({
-    hasTourKey: !!process.env.TOUR_API_SERVICE_KEY,
-    hasBusKey: !!process.env.BUS_API_SERVICE_KEY,
-    hasKakaoMap: !!process.env.NEXT_PUBLIC_KAKAO_MAP_KEY,
-    hasKakaoClient: !!process.env.KAKAO_CLIENT_ID,
-    hasAuthSecret: !!process.env.AUTH_SECRET || !!process.env.NEXTAUTH_SECRET,
-    tourKeyLength: process.env.TOUR_API_SERVICE_KEY?.length || 0,
-    busKeyLength: process.env.BUS_API_SERVICE_KEY?.length || 0
+    busResponse: busResponse.substring(0, 500)
   });
 }
