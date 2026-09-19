e87ec848d97c7f13990a0958f2fcac04ced7d190
crackerking <crackerking@naver.com>
2026-09-19 16:48:58 +0900

    Update debug to check auth secrets

--- app/api/debug-env/route.ts
+++ app/api/debug-env/route.ts
@@ -1,15 +1,10 @@
 import { NextResponse } from 'next/server';
-
 export async function GET() {
-  const serviceKey = process.env.BUS_API_SERVICE_KEY;
-  let busResponse = '';
-  try {
-    const res = await fetch('http://apis.data.go.kr/6290000/busrouteinfo/getrouteinfo?serviceKey=' + serviceKey);
-    busResponse = await res.text();
-  } catch(e: any) {
-    busResponse = e.message;
-  }
   return NextResponse.json({
-    busResponse: busResponse.substring(0, 500)
+    kakaoId: !!process.env.KAKAO_CLIENT_ID,
+    kakaoSecret: !!process.env.KAKAO_CLIENT_SECRET,
+    authSecret: !!process.env.AUTH_SECRET,
+    nextAuthSecret: !!process.env.NEXTAUTH_SECRET,
+    secretLength: (process.env.AUTH_SECRET || process.env.NEXTAUTH_SECRET || '').length
   });
 }
